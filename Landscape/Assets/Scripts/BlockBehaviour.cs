ae20dae6b582f19f0b4dddc397829d81bec45bde
Hythlodaeus42 <brendan_nicholson@yahoo.com.au>
2017-10-19 23:19:04 +1100

    mouse and gestures working on matrix

--- Landscape/Assets/Scripts/BlockBehaviour.cs
+++ Landscape/Assets/Scripts/BlockBehaviour.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlockBehaviour : MonoBehaviour
 {
     private bool selected = false;
-    private Behaviour halo;
+
 
     // Use this for initialization
     void Start()
@@ -21,12 +22,26 @@ public class BlockBehaviour : MonoBehaviour
 
     void OnSelect()
     {
-        Debug.Log("NodeBehaviour.OnSelect() called");
+        Debug.Log("BlockBehaviour.OnSelect() called");
 
         // set selected
         selected = !selected;
 
-        halo = (Behaviour)this.GetComponent("Halo");
-        halo.enabled = selected;
+        Color clr1 = gameObject.transform.Find("Canvas/Panel1").GetComponent<Image>().color;
+        Color clr2 = gameObject.transform.Find("Canvas/Panel2").GetComponent<Image>().color;
+
+        Debug.Log(clr1.ToString());
+
+        if (selected)
+        {
+            gameObject.transform.Find("Canvas/Panel1").GetComponent<Image>().color = new Color(clr1.r, clr1.g, clr1.b, 0.4f);
+            gameObject.transform.Find("Canvas/Panel2").GetComponent<Image>().color = new Color(clr1.r, clr1.g, clr1.b, 0.4f);
+        }
+        else
+        {
+            gameObject.transform.Find("Canvas/Panel1").GetComponent<Image>().color = new Color(clr1.r, clr1.g, clr1.b, 0f);
+            gameObject.transform.Find("Canvas/Panel2").GetComponent<Image>().color = new Color(clr1.r, clr1.g, clr1.b, 0f);
+        }
+
     }
 }
