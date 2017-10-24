124db7e3096ab92539df9b5f7768f4f893fe898a
Hythlodaeus42 <brendan_nicholson@yahoo.com.au>
2017-10-24 23:25:47 +1100

    toggle layer WIP

--- Landscape/Assets/Scripts/BorderBehaviour.cs
+++ Landscape/Assets/Scripts/BorderBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BorderBehaviour : MonoBehaviour {
     private bool selected;
@@ -11,7 +12,21 @@ public class BorderBehaviour : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void OnSelect () {
         Debug.Log("border clicked");
-	}
+        selected = !selected;
+
+        Canvas canvas = this.GetComponentInParent<Canvas>();
+        Image img = canvas.transform.Find("Panel").GetComponent<Image>();
+
+        if (selected)
+        {
+            //clr = 
+            img.color = new Color(img.color.r, img.color.g, img.color.b, 0f);
+        } else
+        {
+            img.color = new Color(img.color.r, img.color.g, img.color.b, 0.1f);
+        }
+
+    }
 }
