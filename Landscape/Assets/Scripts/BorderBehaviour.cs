85c95ec0aabc52d99d8c1c35610512d90f74ac5c
Hythlodaeus42 <brendan_nicholson@yahoo.com.au>
2017-10-24 22:43:28 +1100

    BorderBehaviour.cs WIP

---
+++ Landscape/Assets/Scripts/BorderBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderBehaviour : MonoBehaviour {
+    private bool selected;
+
+    // Use this for initialization
+    void Start () {
+        selected = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
