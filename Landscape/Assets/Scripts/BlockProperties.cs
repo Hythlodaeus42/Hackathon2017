9ff14f15b8070a77ede9e4f759d05db48d3364f1
nd28641 <32885745+nd28641@users.noreply.github.com>
2017-10-24 22:47:11 +1100

    Block properties

---
+++ Landscape/Assets/Scripts/BlockProperties.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockProperties : MonoBehaviour
+{
+    public string ApplicationName;
+    public string BusinessGroup;
+    public string BusinessFunction;
+    public string Desirability;
+}
